a5b4553a6aff7353dfe52f7f23886d4c727daabe
musenwill <musenwill@qq.com>
2019-06-24 23:26:36 +0800

    add benchmark and example

--- test/workerpool_test.go
+++ test/workerpool_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"fmt"
+	"sync"
 	"testing"
 
 	"github.com/musenwill/experimentgo/api"
@@ -17,13 +19,29 @@ func TestWorkerPoolNormal(t *testing.T) {
 	workers.Wait(func() { t.Log("workers finished") })
 }
 
-func TestWorkerPoolManyWork(t *testing.T) {
+func BenchmarkWorkerPool(b *testing.B) {
 	workers := api.NewWorkerPool(1000)
-	for i := 0; i < 100000; i++ {
-		index := i
+	for i := 0; i < b.N; i++ {
 		workers.Excute(func() {
-			t.Log("worker num ", index)
 		})
 	}
-	workers.Wait(func() { t.Log("workers finished") })
+	workers.Wait(nil)
+}
+
+func ExampleWorkerPool() {
+	workers := api.NewWorkerPool(10)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100; i++ {
+			workers.Excute(func() {
+				// do something here
+			})
+		}
+	}()
+	wg.Wait()
+	workers.Wait(func() { fmt.Println("workers finished") })
+	// Output:
+	// workers finished
 }
