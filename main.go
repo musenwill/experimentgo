c53978443da25a791494c4376553e7109dcf9f7c
musenwill <musenwill@11.com>
2019-06-30 00:24:56 +0800

    add makefile

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+var (
+	// Version should be a git tag
+	Version = ""
+	// BuildTime in example: 2019-06-29T23:23:09+0800
+	BuildTime = ""
+)
+
+// check Version and BuildTime
+func init() {
+	if len(Version) <= 0 {
+		panic("Version unset, expect be set with a git tag")
+	}
+	if len(BuildTime) <= 0 {
+		panic("BuildTime unset, expect to be like '2019-06-29T23:23:09+0800'")
+	}
+}
+
 func main() {
 
 }
