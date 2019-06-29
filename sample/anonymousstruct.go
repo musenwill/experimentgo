10e83a263364dbd9bb008c80bb5924768a6262d1
musenwill <musenwill@qq.com>
2019-06-30 00:58:55 +0800

    add some samples

---
+++ sample/anonymousstruct.go
@@ -0,0 +1,8 @@
+package sample
+
+var method = struct {
+	GET    string
+	POST   string
+	PUT    string
+	DELETE string
+}{"GET", "POST", "PUT", "DELETE"}
---
+++ sample/clockserver.go
@@ -0,0 +1,37 @@
+package sample
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+// use command `nc localhost 80001` to monitor client
+
+func start() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn) // handle one connection at a time
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
