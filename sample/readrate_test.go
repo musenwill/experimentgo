0c5a2957b7cb32a1c73b2856cfdcda1b81433d31
musenwill <musenwill@qq.com>
2019-07-16 23:10:56 +0800

    add race compare between RWMutex and channel, channel is much slower

---
+++ sample/readrate_test.go
@@ -0,0 +1,103 @@
+package sample
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+/*
+const read value 100 concurrently 10000 per routine cost 114.117µs
+rwlock read value 100 concurrently 10000 per routine cost 30.362191ms
+channel read value 100 concurrently 10000 per routine cost 255.886359ms
+
+下面三组测试中，都是计算每个 goroutine 各自循环读取数据 10000 次所需要的时间。
+第一组测试，是直接读取常量，没有任何并发同步问题。
+第二组测试，是通过读锁来控制并发问题。
+第三组测试，是通过 channel 来控制并发问题。
+
+很显然的，不用考虑并发控制时，并发读数据的性能是最高的，只需要 114µs
+使用读写锁时，性能下降很严重，需要 30ms，慢了 266 倍
+使用 channel 时，因为 goroutine 都被串行化了，速度更慢，需要 256ms，比读写锁慢 8 倍
+*/
+
+func TestConcurrentReadConst(t *testing.T) {
+	const val = 99
+	start := make(chan struct{})
+	var wg = &sync.WaitGroup{}
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			<-start
+			for j := 0; j < 10000; j++ {
+				if exp, act := 99, val; exp != act {
+					t.Errorf("got %v expected %v", act, exp)
+				}
+			}
+		}()
+	}
+
+	s := time.Now()
+	close(start)
+	wg.Wait()
+	fmt.Printf("const read value %v concurrently %v per routine cost %v\n", 100, 10000, time.Since(s))
+}
+
+func TestConcurrentReadRWLock(t *testing.T) {
+	const val = 99
+	var lock = &sync.RWMutex{}
+
+	start := make(chan struct{})
+	var wg = &sync.WaitGroup{}
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			<-start
+			for j := 0; j < 10000; j++ {
+				lock.RLock()
+				v := val
+				lock.RUnlock()
+				if exp, act := 99, v; exp != act {
+					t.Errorf("got %v expected %v", act, exp)
+				}
+			}
+		}()
+	}
+
+	s := time.Now()
+	close(start)
+	wg.Wait()
+	fmt.Printf("rwlock read value %v concurrently %v per routine cost %v\n", 100, 10000, time.Since(s))
+}
+
+func TestConcurrentReadChannel(t *testing.T) {
+	val := make(chan int)
+	go func() {
+		for {
+			val <- 99
+		}
+	}()
+
+	start := make(chan struct{})
+	var wg = &sync.WaitGroup{}
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			<-start
+			for j := 0; j < 10000; j++ {
+				if exp, act := 99, <-val; exp != act {
+					t.Errorf("got %v expected %v", act, exp)
+				}
+			}
+		}()
+	}
+
+	s := time.Now()
+	close(start)
+	wg.Wait()
+	fmt.Printf("channel read value %v concurrently %v per routine cost %v\n", 100, 10000, time.Since(s))
+}
